b39a972e033fc9dfdd8f318aa9146bd67eb26a0b
Cam C <ckc.cameron@gmail.com>
2020-07-10 10:29:29 -0600

    Write spec for total contestants count on projects show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe "projects show page" do
     expect(page).to have_content("Material: Cardboard Boxes")
     expect(page).to have_content("Challenge Theme: Recycled Material")
   end
+
+  it "counts number of contestants who have participated in a project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+    expect(page).to have_content(recycled_material_challenge.theme)
+    expect(page).to have_content("Number of Contestants: 2")
+
+    visit "/projects/#{upholstery_tux.id}"
+
+    expext(page).to have_content(upholstery_tux.name)
+    expext(page).to have_content(upholstery_tux.material)
+    expext(page).to have_content(furniture_challenge.theme)
+    expect(page).to have_content("Number of Contestants: 1")
+  end
 end
