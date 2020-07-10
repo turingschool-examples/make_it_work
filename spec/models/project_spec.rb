c0ad6cc9988f6747c92bcac25ba3a56800660681
Sienna Kopf <princess.kopf@gmail.com>
2020-07-10 11:40:32 -0600

    Add model test for add_contestant method for projects

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -27,5 +27,17 @@ RSpec.describe Project, type: :model do
 
       expect(news_chic.contestant_experience).to eq(12.5)
     end
+
+    it "can add an existing contestant to an existing project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      news_chic.add_contestant(jay.id)
+      expect(news_chic.contestants).to eq([jay])
+    end
   end
 end
