a6e590cf588e8a3868b4bf2b04e1f6d87a7c13bb
Rion Hutsell <st.gish@gmail.com>
2020-07-10 10:13:04 -0600

    Complete User story 1

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,13 +10,25 @@ require "rails_helper"
 RSpec.describe "Project show page" do
   it "shows project's name, material, and theme of the challenge" do
     recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-    visit '/projects/:id'
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    visit "/projects/#{news_chic.id}"
 
     expect(page).to have_content("News Chic")
-    expect(page).to have_content("Newspaper")
-    expect(page).to have_content("Recycled Material")
+    expect(page).to have_content("Material: Newspaper")
+    expect(page).to have_content("Challenge Theme: Recycled Material")
 
 
   end
