f135456e4f22cc23a307669c95330b3884fe0b6d
Gaby Mendez <gmendez90@gmail.com>
2020-07-10 12:51:52 -0400

    Write model spec for project#avg_experience

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe '#avg_experience' do
+    it 'can calculate average years of experience for it\'s contestants' do
+
+      recycled_material_challenge = Challenge.create!(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create!(name: "News Chic", material: "Newspaper")
+
+      news_chic.contestants.create!(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      news_chic.contestants.create!(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      expect(news_chic.contestants.count).to eq(2)
+      expect(news_chic.avg_experience).to eq(12.5)
+    end
+  end
 end
