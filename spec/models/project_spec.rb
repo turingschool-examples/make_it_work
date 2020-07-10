db5574de04e348db22af8dba3952f7938eda5d82
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 10:27:24 -0600

    Add spec for project model instance method #average_contestant_experience

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it "#average_contestant_experience" do
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp Shade")
+
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: lit_fit.id)
+      ContestantProject.create(contestant_id: erin.id, project_id: lit_fit.id)
+
+      expect(lit_fit.average_contestant_experience).to eq(11.66)
+    end
+  end
 end
