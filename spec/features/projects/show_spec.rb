bab4b2ac44768cafd88d026f7e28627f4871215f
Sienna Kopf <princess.kopf@gmail.com>
2020-07-10 10:30:44 -0600

    Update feature test for projects show page to display the number of contestants

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,13 +10,27 @@ RSpec.describe 'projects show page' do
 
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @recycled_material_challenge.id)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @recycled_material_challenge.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @recycled_material_challenge.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
-  it 'can display name, material, and challenge theme for a particular project' do
+  it 'can display name, material, challenge theme, and number of contestants for a particular project' do
     visit "/projects/#{@news_chic.id}"
 
     expect(page).to have_content(@news_chic.name)
     expect(page).to have_content("Material: #{@news_chic.material}")
-    expect(page).to have_content("Challenge Theme: #{@recycled_material_challenge.theme}")
+    expect(page).to have_content("Challenge Theme: Recycled Material")
+    expect(page).to have_content("Number of Contestants: 3")
   end
 end
