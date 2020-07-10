efeb88c9814fa90c281939438d4f094d4eebd2d1
Gaby Mendez <gmendez90@gmail.com>
2020-07-10 12:31:08 -0400

    Edit projects#show spec for more specificity

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,6 +3,12 @@ RSpec.describe 'as a visitor' do
     before :each do
       @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
       @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
     end
 
     it 'I see that project\'s name and material,
@@ -11,15 +17,15 @@ RSpec.describe 'as a visitor' do
       visit "/projects/#{@news_chic.id}"
 
       expect(page).to have_content('News Chic')
-      expect(page).to have_content('Newspaper')
-      expect(page).to have_content('Recycled Material')
+      expect(page).to have_content('Material: Newspaper')
+      expect(page).to have_content('Challenge Theme: Recycled Material')
     end
 
     it 'I see a count of the number of contestants on this project' do
 
       visit "/projects/#{@news_chic.id}"
 
-      expect(page).to_have_content("Number of Contestants: #{@news_chic.contestants.count}")
+      expect(page).to have_content("Number of Contestants: 2")
     end
   end
 end
