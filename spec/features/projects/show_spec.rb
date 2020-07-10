64b8c4e71df3b9734e7de7c204b3f86201773f08
Cam C <ckc.cameron@gmail.com>
2020-07-10 11:45:04 -0600

    Delete add contestant test due to repetition and moving to contestants new spec

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -59,29 +59,4 @@ RSpec.describe "projects show page" do
 
     expect(page).to have_content("Average Contestant Experience(in years): 11.0")
   end
-
-  it "displays form to add a contestant" do
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-
-    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-
-    visit "projects/#{news_chic.id}"
-
-    click_on "Add Contestant To Project"
-    expect(current_path).to eq("/projects/#{news_chic.id}/contestants/new")
-  end
 end
-
-# As a visitor,
-# When I visit a project's show page
-# I see a form to add a contestant to this project
-# When I fill out a field with an existing contestants id
-# And hit "Add Contestant To Project"
-# I'm taken back to the project's show page
-# And I see that the number of contestants has increased by 1
-# And when I visit the contestants index page
-# I see that project listed under that contestant's name
