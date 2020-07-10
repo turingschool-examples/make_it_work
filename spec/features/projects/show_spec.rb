3a44b0e6b830cd1e3519c0795f9660e8e3eec620
Gaby Mendez <gmendez90@gmail.com>
2020-07-10 11:12:22 -0400

    Write project show_spec

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'as a visitor' do
+  describe 'When I visit a project\'s show page ("/projects/:id")' do
+    before :each do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    end
+
+    it 'I see that project\'s name and material,
+    and I also see the theme of the challenge that this project belongs to.' do
+
+      visit "/projects/#{@news_chic.id}"
+
+      expect(page).to have_content('News Chic')
+      expect(page).to have_content('Newspaper')
+      expect(page).to have_content('Recycled Material')
+
+    end
+  end
+end
