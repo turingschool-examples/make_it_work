4af619bad5a173c8b28eedcaff53d64376cd5198
Gaby Mendez <gmendez90@gmail.com>
2020-07-10 11:29:30 -0400

    Create projects controller and #show route

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'as a visitor' do
   describe 'When I visit a project\'s show page ("/projects/:id")' do
     before :each do
       @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-      @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     end
 
     it 'I see that project\'s name and material,
@@ -13,7 +13,6 @@ RSpec.describe 'as a visitor' do
       expect(page).to have_content('News Chic')
       expect(page).to have_content('Newspaper')
       expect(page).to have_content('Recycled Material')
-
     end
   end
 end
