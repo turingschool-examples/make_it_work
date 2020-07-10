9316ab79deadcb4fe73ab710640f1e48acddbbcf
Nick Edwin <nickedwin85@gmail.com>
2020-07-10 09:58:33 -0600

    projects display route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/projects/:id', to: 'challenges#show'
+
+  get '/contestants', to: 'projects#show'
 end
