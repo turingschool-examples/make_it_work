bc3458563a344b6d8dd34859f3f1427840b29018
Gaby Mendez <gmendez90@gmail.com>
2020-07-10 12:58:15 -0400

    Write Project#avg_experience method

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,11 @@ class Project <ApplicationRecord
 
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def avg_experience
+    years_for_each = contestants.map do |contestant|
+      contestant.years_of_experience
+    end
+    years_for_each.sum.fdiv(years_for_each.count)
+  end
 end
