02507aa782bc40f705cee9207b7e09324503354b
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 09:26:25 -0600

    Add spec for user story 1, project show page

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Project Show Page Spec" do
+  before :each do
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp Shade")
+  end
+
+  describe "As a visitor" do
+    describe "When I visit a project's show page" do
+      it "I see that project's name and material and challenge theme" do
+        visit "/projects/#{@lit_fit.id}"
+
+        expect(page).to have_content(@lit_fit.name)
+        expect(page).to have_content("Material: #{@lit_fit.material}")
+        expect(page).to have_content("Challenge Theme: #{@furniture_challenge.theme}")
+      end
+    end
+  end
+end
