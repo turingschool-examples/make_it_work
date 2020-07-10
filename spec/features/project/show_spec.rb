890635765d91f42a0fdae0a075351f89c17771fc
Dan Sehl <dan.sehl@gmail.com>
2020-07-10 11:52:57 -0600

    Add allow user to add contestant to a project test to project show_spec

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "Project's show page" do
     visit "/projects/#{@lit_fit.id}"
 
     within '.contestant-details' do
-      expect(page).to have_content(@lit_fit.contestants.count)
+      expect(page).to have_content(3)
     end
   end
 
@@ -52,6 +52,38 @@ RSpec.describe "Project's show page" do
 
     within '.contestant-details' do
       expect(page).to have_content(@lit_fit.contestants.average_years_of_experience)
+    end
+  end
+
+  it "allows the user to add a contestant to this project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: @lit_fit.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: @lit_fit.id)
+
+    visit "/projects/#{@lit_fit.id}"
+
+    within '.contestant-details' do
+      expect(page).to have_content(2)
+    end
+
+    within '.form' do
+      fill_in("Id", :with => "#{jay.id}")
+      click_on "Add Contestant To Project"
+    end
+
+    expect(current_path).to eq("/projects/#{@lit_fit.id}")
+
+    within '.contestant-details' do
+      expect(page).to have_content(3)
+    end
+
+    visit "/contestants"
+
+    within '.contestant-details' do
+      expect(page).to have_content("Jay McCarroll")
+      expect(page).to have_content("Litfit")
     end
   end
 end
