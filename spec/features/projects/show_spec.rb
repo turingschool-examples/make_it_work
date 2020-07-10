82d2bb6c621b9145f851e348b49f0ea6fb3531de
Norma Lopez <norma_l304@yahoo.com>
2020-07-10 11:51:24 -0600

    Add test for total number of contestants per project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe "project show page" do
     expect(page).to have_content(project1.material)
     expect(page).to have_content(challenge1.theme)
   end
+
+  it "shows total number of contestants" do
+    jay = Contestant.create!(name: "Jay McCarroll", age: "31", hometown: "San Diego", years_of_experience: 3)
+    victor = Contestant.create!(name: "Victor Ramirez", age: "25", hometown: "Tucson", years_of_experience: 6)
+
+    crafts = Challenge.create!(theme: "Crafts", project_budget: 200)
+
+    leatherfeather = crafts.projects.create!(name: "LeatherFeather", material: "Duct Tape")
+
+    ContestantProject.create!(contestant: jay, project: leatherfeather)
+    ContestantProject.create!(contestant: victor, project: leatherfeather)
+
+    visit "/projects/#{leatherfeather.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
+  end
 end
