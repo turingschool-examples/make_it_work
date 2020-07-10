c409efee2e75379e657a7d994b6d076420aa44c8
Cam C <ckc.cameron@gmail.com>
2020-07-10 10:56:12 -0600

    Relo avg_years_experience method test to contestant model to fix undefined method error

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe Contestant, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
@@ -12,5 +11,23 @@ RSpec.describe Contestant, type: :model do
   describe "relationships" do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
+  end
+
+  describe "methods" do
+    it "calculates .avg_years_experience" do
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+      expect(news_chic.avg_years_experience).to eq(11)
+    end
   end
 end
