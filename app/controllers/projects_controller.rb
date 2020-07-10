83891c9e7e5199d8800d2413ac49988616e09d9c
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 10:55:29 -0600

    Complete User Story Extension 2 - Adding a contestant to a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def update
+    ContestantProject.create(contestant_id: params[:add_contestant_id], project_id: params[:id])
+    redirect_to "/projects/#{params[:id]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/projects/:id", to: "projects#show"
+  post "/projects/:id", to: "projects#update"
 
   get "/contestants", to: "contestants#index"
 
