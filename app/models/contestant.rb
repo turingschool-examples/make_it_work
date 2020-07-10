0c7928570709f4b46add39da7c35188a8bd82e3e
Sienna Kopf <princess.kopf@gmail.com>
2020-07-10 10:23:31 -0600

    Add projects list method to model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -3,4 +3,10 @@ class Contestant <ApplicationRecord
 
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def projects_list
+    projects.map do |project|
+      project.name
+    end
+  end
 end
