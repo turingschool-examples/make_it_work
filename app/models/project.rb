81852c39e99309156017e69ac2f1810c3cfad1d0
Michael Evans <michaeljevans113@gmail.com>
2020-07-10 10:49:48 -0600

    Add #total_contestants method

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def total_contestants
+    contestants.count
+  end
 end
