0227dd1bc2b8d84967263e86231eb51fb88c3db6
twarbelow <taija.warbelow@google.com>
2020-07-10 10:30:33 -0700

    update show page, update contestant with slef method for averaging xp

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,8 @@ class Contestant <ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def self.average_xp
+    average(:years_of_experience)
+  end
 end
