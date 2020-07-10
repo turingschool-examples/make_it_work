07b82bc0298bde1c0a5dcd689c571db42da951e6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-10 13:55:20 -0400

    Make it (a little) prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+  get '/projects', to: 'projects#index'
   get '/projects/:id', to: 'projects#show'
   get 'contestants', to: 'contestants#index'
   patch '/projects/:id', to: 'projects#update'
