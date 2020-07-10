2d1f33d5619e40735309b9db846f52216f4c0eb8
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 10:46:11 -0600

    Add spec for adding a contestant to a project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Project Show Page Spec" do
     @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
     @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
 
     ContestantProject.create(contestant_id: @gretchen.id, project_id: @lit_fit.id)
     ContestantProject.create(contestant_id: @kentaro.id, project_id: @lit_fit.id)
@@ -25,6 +25,21 @@ RSpec.describe "Project Show Page Spec" do
         expect(page).to have_content("Challenge Theme: Apartment Furnishings")
         expect(page).to have_content("Number of Contestants: 3")
         expect(page).to have_content("Average Contestant Experience: 11.67 years")
+      end
+
+      it "I can add a contestant to this project" do
+        visit "/projects/#{@lit_fit.id}"
+        expect(page).to have_content("Number of Contestants: 3")
+
+        fill_in :add_contestant_id, with: @jay.id
+
+        click_button "Add Contestant To Project"
+
+        expect(current_path).to eq("/projects/#{@lit_fit.id}")
+        expect(page).to have_content("Number of Contestants: 4")
+
+        visit "/contestants"
+        expect(page).to have_content("Jay McCarroll\nProjects: Litfit")
       end
     end
   end
