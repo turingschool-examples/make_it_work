30c1a3497b379425fbaf0a7d60b19e37e029db87
Norma Lopez <norma_l304@yahoo.com>
2020-07-10 11:54:51 -0600

    Add test for #total_contestants

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+  describe 'methods' do
+    it '#total_contestants' do
+      jay = Contestant.create!(name: "Jay McCarroll", age: "31", hometown: "San Diego", years_of_experience: 3)
+      victor = Contestant.create!(name: "Victor Ramirez", age: "25", hometown: "Tucson", years_of_experience: 6)
+
+      crafts = Challenge.create!(theme: "Crafts", project_budget: 200)
+
+      leatherfeather = crafts.projects.create!(name: "LeatherFeather", material: "Duct Tape")
+
+      ContestantProject.create!(contestant: jay, project: leatherfeather)
+      ContestantProject.create!(contestant: victor, project: leatherfeather)
+
+      expect(leatherfeather.total_contestants).to eq(2)
+    end
+  end
 end
