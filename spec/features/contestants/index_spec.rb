8056b066fb64c2de79299c0e73af7a46cc0d0c90
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-10 10:03:35 -0600

    Add contestant index spec

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Contestant Index Page" do
+  before :each do
+    @challenge1        = Challenge.create!(theme: "Dress", project_budget: 500)
+
+    @recycled_material = @challenge1.projects.create!(name: "Recycled Material", material: "Bottle")
+    @bridal_wear       = @challenge1.projects.create!(name: "Bridal Wear", material: "Leaf")
+    @apartment         = @challenge1.projects.create!(name: "Apartment Furnishings", material: "lamp Shade")
+
+    @bob               = Contestant.create!(name: 'Bob', age: 32, hometown: 'Denver', years_of_experience: 9)
+    @sara              = Contestant.create!(name: 'Sara', age: 26, hometown: 'Salt Lake City', years_of_experience: 4)
+    @olivia            = Contestant.create!(name: 'Olivia', age: 25, hometown: 'Centennial', years_of_experience: 7)
+
+    ContestantProject.create!(contestant: @bob, project: @recycled_material)
+    ContestantProject.create!(contestant: @bob, project: @apartment)
+    ContestantProject.create!(contestant: @sara,project: @recycled_material)
+    ContestantProject.create!(contestant: @sara,project: @bridal_wear)
+    ContestantProject.create!(contestant: @olivia, project: @recycled_material)
+    ContestantProject.create!(contestant: @olivia, project: @bridal_wear)
+    ContestantProject.create!(contestant: @olivia, project: @apartment)
+  end
+
+  it "can show a list of all contestants name and a list of their projects" do
+
+    visit "/contestant"
+
+    expect(page).to have_content(@bob.name)
+    expect(page).to have_content(@olivia.name)
+    expect(page).to have_content(@sara.name)
+
+    expect(page).to have_content(@bob.projects.name)
+    expect(page).to have_content(@olivia.projects.name)
+    expect(page).to have_content(@sara.projects.name)
+  end
+
+end
+
+# I see a list of names of all the contestants
+# And under each contestants name I see a list of the projects (names) that they've been on
+# ​
+# (e.g.   Kentaro Kameyama
+#         Projects: Litfit, Rug Tuxedo
