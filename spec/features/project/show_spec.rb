6efd632c77b48a54c9ace2a91dae8c27cfd43927
Dan Sehl <dan.sehl@gmail.com>
2020-07-10 09:59:15 -0600

    Refactor project show_spec to have a before :each and new it block to check for a project challenge's theme

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Project's show page", type: :feature do
-  it "displays project's name and material" do
-    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  before :each do
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  end
 
-    visit "/projects/#{lit_fit.id}"
+  it "displays project's name and material" do
+    visit "/projects/#{@lit_fit.id}"
 
     within '.project-details' do
-      expect(page).to have_content(lit_fit.name)
-      expect(page).to have_content(lit_fit.material)
+      expect(page).to have_content(@lit_fit.name)
+      expect(page).to have_content(@lit_fit.material)
+    end
+  end
+
+  it "displays the project's challenge theme" do
+    visit "/projects/#{@lit_fit.id}"
+
+    within '.challenge-details' do
+      expect(page).to have_content(@furniture_challenge.theme)
     end
   end
 end
