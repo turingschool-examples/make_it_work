38a51861c8a49a4e9869f29c49fed37b0c9918f8
Dan Sehl <dan.sehl@gmail.com>
2020-07-10 10:41:29 -0600

    Add test for displaying the number of contestants to project show spec

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -22,4 +22,21 @@ RSpec.describe "Project's show page" do
       expect(page).to have_content(@furniture_challenge.theme)
     end
   end
+
+  it "displays the project's number of contestants" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: @lit_fit.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: @lit_fit.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: @lit_fit.id)
+
+
+    visit "/projects/#{@lit_fit.id}"
+
+    within '.contestant-details' do
+      expect(page).to have_content(@lit_fit.contestants.count)
+    end
+  end
 end
