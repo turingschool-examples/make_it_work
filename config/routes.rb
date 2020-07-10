8986a08f43fd7611d284a02b9e8ac094f8b9bf6a
Cam C <ckc.cameron@gmail.com>
2020-07-10 11:38:41 -0600

    Add post route for contestants new action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/projects/:id', to: 'projects#show'
   get '/contestants', to: 'contestants#index'
   get 'projects/:id/contestants/new', to: 'contestants#new'
+  post '/projects/:id/contestants', to: 'contestants#create'
 end
