64e998af9dfcdabcec88f2524104b3eeb4222066
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 10:33:44 -0600

    Add instance method average_contestant_experience to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,9 @@ class Project <ApplicationRecord
 
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def average_contestant_experience
+    total_experience = contestants.map(&:years_of_experience).sum.to_f
+    (total_experience / contestants.size).round(2)
+  end
 end
