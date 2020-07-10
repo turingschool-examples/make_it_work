b025cdea4d558a8f31b5afa0aeec7ee84a3b8525
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-10 12:41:00 -0400

    Change project show spec to use before each and combine tests

--- spec/features/project_show_spec.rb
+++ spec/features/project_show_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'project show page', type: :feature do
+  before(:each) do
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+  end
+
   it 'can access a show page for a given project' do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    visit "/projects/#{@upholstery_tux.id}"
 
-    visit "/projects/#{news_chic.id}"
+    expect(page).to have_content("#{@upholstery_tux.name}")
+    expect(page).to have_content("Material: #{@upholstery_tux.material}")
+    expect(page).to have_content("Challenge Theme: #{@furniture_challenge.theme}")
+  end
 
-    expect(page).to have_content("#{news_chic.name}")
-    expect(page).to have_content("Material: #{news_chic.material}")
-    expect(page).to have_content("Challenge Theme: #{recycled_material_challenge.theme}")
+  it 'can show the number of contestants working on a given project' do
+    visit "/projects/#{@upholstery_tux.id}"
+
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
