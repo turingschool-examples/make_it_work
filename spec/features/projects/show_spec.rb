cf4a0c1a049b647447c3718c4c05cc635ff951ff
Nick Edwin <nickedwin85@gmail.com>
2020-07-10 10:31:48 -0600

    Updated test for number of contestants on a project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,17 +1,22 @@
 RSpec.describe "as a visitor", type: :feature do
+  before :each do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @project_1 = @recycled_material_challenge.projects.create(name: "Litfit", material: "Lamp Shade")
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @project_1.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @project_1.id)
+  end
+
   describe 'when I visit a projects show page' do
     it 'I see that projects name, material and theme' do
 
-      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      visit "/projects/#{@project_1.id}"
 
-      project_1 = recycled_material_challenge.projects.create(name: "Litfit", material: "Lamp Shade")
-
-
-      visit "/projects/#{project_1.id}"
-
-      expect(page).to have_content("Theme: #{recycled_material_challenge.theme}")
-      expect(page).to have_content("Name: #{project_1.name}")
-      expect(page).to have_content("Material: #{project_1.material}")
+      expect(page).to have_content("Theme: #{@recycled_material_challenge.theme}")
+      expect(page).to have_content("Name: #{@project_1.name}")
+      expect(page).to have_content("Material: #{@project_1.material}")
+      expect(page).to have_content("Number of Contestants: 2")
     end
   end
 end
