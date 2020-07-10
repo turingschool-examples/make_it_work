87074bd7f63ac1a6403c4a3a749251f9dffd1feb
Cam C <ckc.cameron@gmail.com>
2020-07-10 11:46:04 -0600

    Add create method to contestants controller

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -6,4 +6,16 @@ class ContestantsController < ApplicationController
   def new
     @project = Project.find(params[:id])
   end
+
+  def create
+    project = Project.find(params[:id])
+    project.contestants.create(contestant_params)
+    redirect_to "/projects/#{project.id}"
+  end
+
+  private
+
+  def contestant_params
+    params.permit(:name, :age, :hometown, :years_of_experience)
+  end
 end
