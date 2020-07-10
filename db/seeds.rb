c0844a29ea28034004c76085d2e9edcbd2d49591
Cam C <ckc.cameron@gmail.com>
2020-07-10 10:10:02 -0600

    Edit seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_b
 
 news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
 boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-
 upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
 lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
