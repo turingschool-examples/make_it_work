705412ae97bc1835977042a92558eb876de24d95
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-10 11:03:53 -0600

    Complete user story 3

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "When I visit a project's show page" do
-  it "I see the project's name and material, and the theme of the challenge the project belongs to" do
+  it "I see the project's name and material, it challenge theme, and its number of contestants" do
     furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
 
-    visit "/projects/#{lit_fit.id}"
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
 
-    expect(page).to have_content(lit_fit.name)
-    expect(page).to have_content("Material: #{lit_fit.material}")
-    expect(page).to have_content("Challenge Theme: #{lit_fit.challenge.theme}")
+    ContestantProject.create(contestant_id: jay.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+    visit "/projects/#{upholstery_tux.id}"
+
+    expect(page).to have_content(upholstery_tux.name)
+    expect(page).to have_content("Material: #{upholstery_tux.material}")
+    expect(page).to have_content("Challenge Theme: #{upholstery_tux.challenge.theme}")
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
