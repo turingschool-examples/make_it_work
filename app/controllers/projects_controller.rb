bedba10b2ca822492509e95ac107faa87f41b65e
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-10 10:12:44 -0600

    Make projects_controller skinny

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,5 @@
 class ProjectsController < ApplicationController
   def show
-    project = Project.find(params[:id])
-    @project_name = project.name
-    @project_material = project.material
-    @project_theme = project.challenge.theme
+    @project = Project.find(params[:id])
   end
 end
