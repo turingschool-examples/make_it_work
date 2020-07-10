700b662308708cb1970ea4991b0226271dc0e788
Dan Sehl <dan.sehl@gmail.com>
2020-07-10 11:53:21 -0600

    Add route for posting to contestants

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/projects/:id', to: 'project#show'
   get '/contestants', to: 'contestant#index'
+  post '/contestants', to: 'contestant_project#create'
 end
