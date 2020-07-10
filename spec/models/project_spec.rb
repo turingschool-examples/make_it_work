03e90acdda7df78342887cd4d6fb4d025ef52683
Cam C <ckc.cameron@gmail.com>
2020-07-10 10:49:37 -0600

    Write spec for avg_years_experience method in project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -30,7 +30,22 @@ RSpec.describe Project, type: :model do
 
       expect(news_chic.total_contestants).to eq(2)
       expect(upholstery_tux.total_contestants).to eq(1)
+    end
 
+    it "calculates .avg_years_experience" do
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+      expect(news_chic.avg_years_experience).to eq(11)
     end
   end
 end
