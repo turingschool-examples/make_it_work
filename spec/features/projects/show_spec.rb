67a315f1ea9a910128b63c11a90d5f0b245f3f7d
Cam C <ckc.cameron@gmail.com>
2020-07-10 10:48:29 -0600

    Write spec for average contestant experience for projects show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -41,4 +41,22 @@ RSpec.describe "projects show page" do
     expect(page).to have_content(furniture_challenge.theme)
     expect(page).to have_content("Number Of Contestants: 1")
   end
+
+  it "displays average years of experience for all contestants per project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content("Average Contestant Experience: 11 years")
+  end
 end
