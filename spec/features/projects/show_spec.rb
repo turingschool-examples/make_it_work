47b22d1498c4a78c8bd236aa3d2ae4515e2347ac
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-10 11:19:30 -0600

    Added test for # of contestants

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,13 +3,21 @@ require 'rails_helper'
 RSpec.describe 'Project show page' do
   it "shows the project's name, material, and theme of the challenge" do
     challenge = Challenge.create(theme: "summer", project_budget: 250)
-    project = Project.create(name: "project 1", material: "denim", challenge_id: challenge.id)
+    news_chic = Project.create(name: "project 1", material: "denim", challenge_id: challenge.id)
 
-    visit "/projects/#{project.id}"
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
     
-    expect(current_path).to eq("/projects/#{project.id}")
-    expect(page).to have_content("Project Name: #{project.name}")
-    expect(page).to have_content("Material: #{project.material}")
-    expect(page).to have_content("Challenge Theme: #{project.challenge.theme}")
+    expect(current_path).to eq("/projects/#{news_chic.id}")
+    expect(page).to have_content("Project Name: #{news_chic.name}")
+    expect(page).to have_content("Material: #{news_chic.material}")
+    expect(page).to have_content("Challenge Theme: #{news_chic.challenge.theme}")
+    expect(page).to have_content("Number of Contestants: #{news_chic.contestants.count}")
+    save_and_open_page
   end
 end
